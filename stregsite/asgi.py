c9a24de2f23713a3a4abdde99689d64f6d289717
Sankeerthan Reddy <54495816+ratnasankeerthanreddy@users.noreply.github.com>
2021-02-09 20:59:34 +0530

    Add files via upload

---
+++ stregsite/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for stregsite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stregsite.settings')
+
+application = get_asgi_application()
